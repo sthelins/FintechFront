49a8d07160f342a476c29fa52e2187933c1b26a0
Sthefany <sthefanylins_albuquerque@hotmail.com>
2022-12-06 23:25:47 -0300

    abertura do menu

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+const nav = document.querySelector('#header .menu')
+const toggle = document.querySelectorAll('nav .toggle')
 
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
